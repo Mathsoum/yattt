bc139051ecd87b3c0303c93d63e9f47fc300dd25
Mathieu SOUM <mathieu.soum@gmail.com>
2017-08-27 21:32:49 +0200

    Hour numbers added to chronology.

--- views.py
+++ views.py
@@ -1,5 +1,6 @@
 from kivy.graphics.context_instructions import Color
-from kivy.graphics.vertex_instructions import Line
+from kivy.graphics.vertex_instructions import Line, Rectangle
+from kivy.core.text import Label as CoreLabel
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.widget import Widget
 
@@ -17,6 +18,8 @@ class Chronology(Widget):
         super(Chronology, self).__init__(**kwargs)
         self.color = None
         self.time_lines = []
+        self.hours_core_labels = []
+        self.hours_rect_labels = []
         self.draw()
 
     def draw(self):
@@ -28,20 +31,43 @@ class Chronology(Widget):
             self.color = Color(rgba=(1, 0, 0, 1.))
 
             self.time_lines = [Line(**line_kwargs) for i in range(self.TIME_INTERVAL_COUNT + 1)]
+            self.hours_core_labels = []
+            self.hours_rect_labels = []
+            for hour in range(24 + 1):
+                label = CoreLabel()
+                label.text = "%02d" % hour
+                label.refresh()
+                self.hours_core_labels.append(label)
+                self.hours_rect_labels.append(Rectangle(size=label.size, texture=label.texture))
 
     def update_lines(self):
         margin_hours_height = self.height * 0.1
         margin_halves_height = self.height * 0.4
         for i in range(self.TIME_INTERVAL_COUNT + 1):
             if (i % 2) == 0:
+                # Hours
+                p1_x = self.x + (((self.width * 0.05) / 2) + (self.width * 0.95 * i) / self.TIME_INTERVAL_COUNT)
+                p1_y = self.y + margin_hours_height + self.height * 0.18
+                p2_x = self.x + (((self.width * 0.05) / 2) + (self.width * 0.95 * i) / self.TIME_INTERVAL_COUNT)
+                p2_y = self.y + self.height - margin_hours_height
                 self.time_lines[i].points = [
-                    self.x + (((self.width * 0.05) / 2) + (self.width * 0.95 * i) / self.TIME_INTERVAL_COUNT), self.y + margin_hours_height,
-                    self.x + (((self.width * 0.05) / 2) + (self.width * 0.95 * i) / self.TIME_INTERVAL_COUNT), self.y + self.height - margin_hours_height,
+                    p1_x, p1_y,
+                    p2_x, p2_y,
+                ]
+                label = self.hours_core_labels[int(i / 2)]
+                self.hours_rect_labels[int(i / 2)].pos = [
+                    p1_x - (label.width / 2),
+                    p1_y - label.height - (self.height * 0.02)
                 ]
             else:
+                # Half hours
+                p1_x = self.x + (((self.width * 0.05) / 2) + (self.width * 0.95 * i) / self.TIME_INTERVAL_COUNT)
+                p1_y = self.y + margin_halves_height - (self.height * 0.05)
+                p2_x = self.x + (((self.width * 0.05) / 2) + (self.width * 0.95 * i) / self.TIME_INTERVAL_COUNT)
+                p2_y = self.y + self.height - margin_halves_height - (self.height * 0.05)
                 self.time_lines[i].points = [
-                    self.x + (((self.width * 0.05) / 2) + (self.width * 0.95 * i) / self.TIME_INTERVAL_COUNT), self.y + margin_halves_height,
-                    self.x + (((self.width * 0.05) / 2) + (self.width * 0.95 * i) / self.TIME_INTERVAL_COUNT), self.y + self.height - margin_halves_height,
+                    p1_x, p1_y,
+                    p2_x, p2_y,
                 ]
 
     def on_pos(self, instance, value):
